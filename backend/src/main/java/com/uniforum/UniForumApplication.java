f3c7ee0a9a3e120ec802ce13e83d9f272b992ac3
jamesak535 <jamesak@naver.com>
2025-03-14 18:26:50 -0500

    setup and tested spring boot

---
+++ backend/src/main/java/com/uniforum/UniForumApplication.java
@@ -0,0 +1,13 @@
+package com.uniforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniForumApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/uniforum/UniForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
